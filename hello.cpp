beee53ac66da4456865e3eb6c30ee79f992227ba
Sindhu Thanikonda <sindhu-thanikonda@users.noreply.github.com>
2019-05-06 23:16:14 -0500

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// Basic Hello World
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  cout<<"Hello World"; // Standard output stream 
+  return 0; //int main() where int is return value
+}
---
+++ helloname.cpp
@@ -0,0 +1,14 @@
+// Simple Hello Name
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  string s1;
+  cout<<"Enter any Name: ";
+  cin >> s1;
+  cout <<"Hello" <<s1 <<"\n";
+  return 0; //int main() where int is return value
+}
